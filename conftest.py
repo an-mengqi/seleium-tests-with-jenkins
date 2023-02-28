aff294a4ea48a2878f6f4f715bbce1830cdd8ea3
Anastasiia Monakhova <a.klopova@dssl.ru>
2023-02-28 20:01:56 +0100

    conftest changed

--- conftest.py
+++ conftest.py
@@ -22,7 +22,7 @@ def browser(request):
     log_level = request.config.getoption("--log_level")
 
     logger = logging.getLogger(request.node.name)
-    file_handler = logging.FileHandler(f"logs/{request.node.name}.log")
+    file_handler = logging.FileHandler(f"seleium-tests-with-jenkins/logs/{request.node.name}.log")
     file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
     logger.addHandler(file_handler)
     logger.setLevel(level=log_level)
